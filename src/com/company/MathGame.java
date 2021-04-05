4d64159c41d92bbfb1f1e1fb50438d0b5f1d3d8c
ARW026 <80724258+ARW026@users.noreply.github.com>
2021-04-05 15:11:51 +1000

    Add files via upload

--- src/com/company/MathGame.java
+++ src/com/company/MathGame.java
@@ -29,7 +29,7 @@ public class MathGame {
         // "response" variable to store the user input
         double response = 0;
 
-
+        // this for loop creates the random numbers for the questions and adds it into the array which is repeated x10
         for (int i = 0; i < 10; i++) {
             randVal1 = rand.nextInt(12) + 1;
             randVal2 = rand.nextInt(12) + 1;
@@ -39,6 +39,7 @@ public class MathGame {
 
         while (!arrayQ.isEmpty()) {
 
+            // Gets the integers from the array and displays it to the user
             System.out.println();
             randVal1 = arrayQ.get(0);
             randVal2 = arrayQ.get(1);
@@ -51,16 +52,20 @@ public class MathGame {
                 System.out.print("Enter answer: ");
                 response = sc.nextInt();
 
-
+                // if the user answers correctly, then the 2 numbers displayed get removed
                 if (correct == response) {
                     count++;
                     System.out.println("That's correct");
                     arrayQ.remove(0);
                     arrayQ.remove(0);
                 } else {
+                    // if the user answers incorrectly, then the 2 numbers displayed get added to the end of the array
+                    // as well as removing the numbers
                     System.out.println("That's incorrect," + " the answer was " + correct + "\n");
                     arrayQ.add(randVal1);
                     arrayQ.add(randVal2);
+                    // index 0 gets removed first which allows index 1 to move up to index 0 which then removes the second number
+                    // that's why both arrayQ.remove have an index of 0, otherwise it would skip numbers
                     arrayQ.remove(0);
                     arrayQ.remove(0);
                 }
